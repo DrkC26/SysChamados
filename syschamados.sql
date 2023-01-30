8210740f4ce66923bdc00c23701cb5d15c04c3a7
Bruno_Collange <69699014+DrkC26@users.noreply.github.com>
2023-01-30 11:40:59 -0300

    Update syschamados.sql

--- syschamados.sql
+++ syschamados.sql
@@ -69,19 +69,8 @@ CREATE TABLE `resolvidos` (
 --
 
 INSERT INTO `resolvidos` (`id`, `problema_r`, `setor_r`, `nome_r`, `comentario_r`, `momento_registro_r`) VALUES
-(7, 'a impressora fica dando falha na conexão, pode vir aq please?', 'adm', 'Duda', '', '2022-09-27 10:17:32'),
-(8, 'IMPRESSORA PAROU', 'ADM', 'KELLY', '', '2022-09-27 13:00:31'),
-(10, 'atolamento de papel ', 'adm', 'nataly', 'SOCORRO BRUNO', '2022-09-27 16:13:09'),
-(11, 'ATOLAMENTO DE ESCANER', 'adm', 'NATALY', 'SOCORRO BRUNO', '2022-09-27 15:36:53'),
-(12, 'a impressora dnv k', 'adm', 'duda', '', '2022-09-28 08:21:07'),
-(13, 'atolamento de papel ', 'adm', 'nataly', 'SOCORRO BRUNO', '2022-09-28 14:45:42'),
-(14, 'atolamento de papel scanner', 'adm', 'nataly', 'SOCORRO BRUNO', '2022-09-28 15:39:42'),
-(15, 'IMPRESSORA', 'RH', 'SABRINA', 'TRAVOU', '2022-10-03 14:49:14'),
-(16, 'IMPRESSORA NAO IMPRIME', 'RH', 'SABRINA', 'SOCORRO DEUS', '2022-10-06 13:56:08'),
-(17, 'TROC CART DANIF', 'adm', 'Nataly', 'SOCORRO BRUNO', '2022-10-10 09:53:48'),
-(18, 'a impressora travou e meu computador não quer abrir documento', 'adm', 'dudinha', '', '2022-10-11 10:14:54'),
-(19, 'PAPEL NA BANDEIJA (NÃO CONSEGUI ARRUMAR)', 'adm', 'nataly', 'SOCORRO BRUNO', '2022-10-11 10:15:14'),
-(21, 'impressora travou', 'adm', 'duda', '', '2022-10-11 16:09:09');
+(7, 'exemplo', 'adm', 'exemplo', 'teste', '2022-09-27 10:17:32'),
+
 
 --
 -- Índices para tabelas despejadas
